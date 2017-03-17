cf0c15a316aace49d15cf3771ec78268e0fe6d0d
gitjcc <jc_jesse@hotmail.com>
2017-03-17 12:43:29 +0800

    feature : add ./src/utils/extend.js

---
+++ src/utils/extend.js
@@ -0,0 +1,38 @@
+
+function extend(out) {
+    out = out || {};
+
+    for (let i = 1; i < arguments.length; i++) {
+        if (!arguments[i])
+            continue;
+
+        for (let key in arguments[i]) {
+            if (arguments[i].hasOwnProperty(key))
+                out[key] = arguments[i][key];
+        }
+    }
+
+    return out;
+};
+
+function deepExtend(out) {
+    out = out || {};
+
+    for (let i = 1; i < arguments.length; i++) {
+        let obj = arguments[i];
+
+        if (!obj)
+            continue;
+
+        for (let key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                if (typeof obj[key] === 'object')
+                    out[key] = deepExtend(out[key], obj[key]);
+                else
+                    out[key] = obj[key];
+            }
+        }
+    }
+
+    return out;
+};
